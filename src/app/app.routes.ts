1d9faaf07529f63a67ee3a909562775371bab4e1
IslamHamada <Islamhamada222@gmail.com>
2025-11-12 17:13:19 +0100

    add Cart route and connect it to the Cart component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import {Home} from './home/home';
+import {Cart} from './cart/cart';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
     title: 'Home page',
+  },
+  {
+    path: 'cart',
+    component: Cart,
+    title: 'Cart',
   },
 ];
