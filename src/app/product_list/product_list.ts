1becf5591658b5f7147d55760b1b84e08dd52029
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 23:46:53 +0100

    inject ProductRestAPI and getAllProduct() from the backend

--- src/app/product_list/product_list.ts
+++ src/app/product_list/product_list.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ProductRestAPI} from '../rest_api/product.restapi';
 
 @Component({
   selector: 'products-list',
   templateUrl: 'product_list.html',
 })
 export class ProductList {
+  productRestService = inject(ProductRestAPI);
 
+  products$ = this.productRestService.getAllProducts();
 }
