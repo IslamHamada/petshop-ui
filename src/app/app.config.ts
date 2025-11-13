6f3d21ca9e40f34ef10d51ed6141d61dfe31af1e
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 23:31:10 +0100

    Add configs to allow the injection of HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideAuth0} from '@auth0/auth0-angular';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
     provideAuth0({
       domain: 'dev-atfpp36qj24tzo8l.us.auth0.com',
