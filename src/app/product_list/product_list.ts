57a488784faa4027933cfa92af5b50c79463e7d3
IslamHamada <Islamhamada222@gmail.com>
2025-11-13 23:47:42 +0100

    list all products in the ProductList component

--- src/app/product_list/product_list.ts
+++ src/app/product_list/product_list.ts
@@ -1,9 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {ProductRestAPI} from '../rest_api/product.restapi';
+import {Product} from '../models/Product';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'products-list',
   templateUrl: 'product_list.html',
+  imports: [
+    AsyncPipe
+  ]
 })
 export class ProductList {
   productRestService = inject(ProductRestAPI);
