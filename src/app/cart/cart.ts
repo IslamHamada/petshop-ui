59e1f312fd9650380a03a1dbe46c9b683470708d
IslamHamada <Islamhamada222@gmail.com>
2025-11-15 01:52:55 +0100

    adding a basic template to view the user's cart items

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,11 +1,15 @@
 import {Component, inject} from '@angular/core'
 import {CartRestAPI} from '../rest_api/cart.restapi';
+import {AsyncPipe} from '@angular/common';
 import {UserService} from '../user.service';
 import {CartItem} from '../models/CartItem';
 
 @Component({
   selector: 'cart',
   templateUrl: 'cart.html',
+  imports: [
+    AsyncPipe
+  ]
 })
 export class Cart {
   cartRestAPI = inject(CartRestAPI);
