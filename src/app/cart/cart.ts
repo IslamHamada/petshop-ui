cced5d3ba84d3d59384d4c6ec879fb8783073eb2
IslamHamada <Islamhamada222@gmail.com>
2025-11-15 01:52:29 +0100

    Injecting two Dependencies + adding cart$
    cart$ emits the cart items of the current user

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core'
+import {Component, inject} from '@angular/core'
+import {CartRestAPI} from '../rest_api/cart.restapi';
+import {UserService} from '../user.service';
+import {CartItem} from '../models/CartItem';
 
 @Component({
   selector: 'cart',
   templateUrl: 'cart.html',
 })
 export class Cart {
-
+  cartRestAPI = inject(CartRestAPI);
+  userService = inject(UserService);
+  cart$  = this.userService.rxOnBackendId$<CartItem[]>(id => this.cartRestAPI.getCartByUserId(id));
 }
