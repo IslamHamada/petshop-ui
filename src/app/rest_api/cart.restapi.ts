64d32c2930ca7b9d8c979f9ac2ee2cfe3dd0bba1
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 12:49:24 +0100

    remove unused import + fix addCartItem + getCartByUserId

--- src/app/rest_api/cart.restapi.ts
+++ src/app/rest_api/cart.restapi.ts
@@ -1,29 +1,22 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {UserService} from '../user.service';
+import {CartItem} from '../models/CartItem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartRestAPI {
-  userService = inject(UserService)
   http = inject(HttpClient);
 
-  addCartItem(product_id: number, count: number) {
-    if(this.userService.loggedIn){
-      this.http.post("http://localhost:9090/cart",{
-        user_id: this.userService.user?.auth0_id,
-        product_id: product_id,
-        count: count
-      });
-      this.http.get<[]>("http://localhost:9090/cart", {params: {user_id : this.userService.user?.backend_id ?? ''}}).subscribe(
-        data => {
-          this.userService.cartItems = data.length
-        }
-      )
-    } else {
-      console.log("local cart not yet implemented");
-    }
+  addCartItem(product_id: number, count: number, backend_id: number) {
+    return this.http.post("http://localhost:9090/cart",{
+      backend_id: backend_id,
+      product_id: product_id,
+      count: count
+    });
+  }
 
+  getCartByUserId(id : number) {
+    return this.http.get<CartItem[]>(`http://localhost:9090/cart/${id}`);
   }
 }
