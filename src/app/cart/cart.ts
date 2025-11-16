e482a9d66250c63ca561532c23c6f9e6570c2748
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 15:25:59 +0100

    remove Async pipe and fetch cart items in ngOnInit()

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,6 +1,5 @@
 import {Component, inject} from '@angular/core'
 import {CartRestAPI} from '../rest_api/cart.restapi';
-import {AsyncPipe} from '@angular/common';
 import {UserService} from '../user.service';
 import {CartItem} from '../models/CartItem';
 import {OrderRestAPI} from '../rest_api/order.restapi';
@@ -9,15 +8,17 @@ import {Order} from '../models/Order';
 @Component({
   selector: 'cart',
   templateUrl: 'cart.html',
-  imports: [
-    AsyncPipe
-  ]
 })
 export class Cart {
   cartRestAPI = inject(CartRestAPI);
   userService = inject(UserService);
   orderRestAPI = inject(OrderRestAPI)
   cart$  = this.userService.rxOnBackendId$<CartItem[]>(id => this.cartRestAPI.getCartByUserId(id));
+  cart : CartItem[] = [];
+
+  ngOnInit() {
+    this.cart$.subscribe(cart => this.cart = cart);
+  }
 
   orderClick(){
     this.userService.rxOnBackendId$<Order>(id => this.orderRestAPI.order(id)).subscribe();
