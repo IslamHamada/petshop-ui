c93d2f52da30a62a20d9fea632f84c7ddfd1931e
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 12:53:37 +0100

    fetch orderHistory$ + import Asyncpipe

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,7 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {UserService} from '../user.service';
+import {OrderRestAPI} from '../rest_api/order.restapi';
+import {Order} from '../models/Order';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'profile',
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: 'profile.html'
 })
-export class Profile{}
+export class Profile{
+  userService = inject(UserService)
+  orderRestAPI = inject(OrderRestAPI)
+  orderHistory$ = this.userService.rxOnBackendId$<Order[]>(id => this.orderRestAPI.getOrderHistory(id));
+}
