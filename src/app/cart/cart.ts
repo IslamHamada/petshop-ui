49c2de66604d5b4b62e756167747b6478075d47f
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 12:57:45 +0100

    add orderClick() for order button listener

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -3,6 +3,8 @@ import {CartRestAPI} from '../rest_api/cart.restapi';
 import {AsyncPipe} from '@angular/common';
 import {UserService} from '../user.service';
 import {CartItem} from '../models/CartItem';
+import {OrderRestAPI} from '../rest_api/order.restapi';
+import {Order} from '../models/Order';
 
 @Component({
   selector: 'cart',
@@ -14,5 +16,10 @@ import {CartItem} from '../models/CartItem';
 export class Cart {
   cartRestAPI = inject(CartRestAPI);
   userService = inject(UserService);
+  orderRestAPI = inject(OrderRestAPI)
   cart$  = this.userService.rxOnBackendId$<CartItem[]>(id => this.cartRestAPI.getCartByUserId(id));
+
+  orderClick(){
+    this.userService.rxOnBackendId$<Order>(id => this.orderRestAPI.order(id)).subscribe();
+  }
 }
