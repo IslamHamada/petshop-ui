2e724e03e1c117dd7df481fbcd43df7d1b36afca
IslamHamada <Islamhamada222@gmail.com>
2025-11-16 12:56:08 +0100

    addToCartClick()

--- src/app/product_list/product_list.ts
+++ src/app/product_list/product_list.ts
@@ -1,7 +1,9 @@
 import {Component, inject} from '@angular/core';
 import {ProductRestAPI} from '../rest_api/product.restapi';
+import {CartRestAPI} from '../rest_api/cart.restapi';
 import {Product} from '../models/Product';
 import {AsyncPipe} from '@angular/common';
+import {UserService} from '../user.service';
 
 @Component({
   selector: 'products-list',
@@ -12,6 +14,17 @@ import {AsyncPipe} from '@angular/common';
 })
 export class ProductList {
   productRestService = inject(ProductRestAPI);
+  cartRestService = inject(CartRestAPI);
+  userService = inject(UserService);
 
   products$ = this.productRestService.getAllProducts();
+
+  addToCartClick(product_id: number){
+    this.userService.cartItems++;
+    if(this.userService.loggedIn){
+      this.userService.rxOnBackendId$(z => this.cartRestService.addCartItem(product_id, 1, z)).subscribe();
+    } else {
+
+    }
+  }
 }
