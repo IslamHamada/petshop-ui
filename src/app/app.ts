5be06e32ae9e28c54e697938e75d14af46190a25
IslamHamada <Islamhamada222@gmail.com>
2025-11-24 23:24:27 +0100

    use mat tags in app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import {Component, inject} from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import {UserService} from './user.service';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatBadge, MatBadgeModule} from '@angular/material/badge';
+import {MatButton} from '@angular/material/button';
+import {MatDivider} from '@angular/material/divider';
 import {SessionService} from './session/session.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbar, MatBadge, MatButton, MatDivider],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
