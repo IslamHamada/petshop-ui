1a18d9abb2a6d313aa8dcd60bd41d73b06b746f1
IslamHamada <Islamhamada222@gmail.com>
2025-11-24 22:46:15 +0100

    add both CheckoutComponent and ProductComponent to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import {Home} from './home/home';
 import {Cart} from './cart/cart';
 import {Profile} from './profile/profile';
+import {ProductComponent} from './product/product.component';
+import {CheckoutComponent} from './checkout/checkout';
 
 export const routes: Routes = [
   {
@@ -13,6 +15,16 @@ export const routes: Routes = [
     path: 'cart',
     component: Cart,
     title: 'Cart',
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
+    title: 'Checkout',
+  },
+  {
+    path: 'product/:id',
+    component: ProductComponent,
+    title: 'Product',
   },
   {
     path: 'profile',
