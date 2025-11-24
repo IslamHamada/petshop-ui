e44a9761b00a0c4489219f997658c87a7a1833a7
IslamHamada <Islamhamada222@gmail.com>
2025-11-24 22:29:25 +0100

    add environment files for both builds, normal and production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  gateway_url: 'https://34.125.78.235:80'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  gateway_url: 'http://localhost:9090'
+}
