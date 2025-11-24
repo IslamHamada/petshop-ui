903087b186c2bb9d51dea0fd0f7676ce0381a146
IslamHamada <Islamhamada222@gmail.com>
2025-11-24 23:44:38 +0100

    update profile.html to use mat tags

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -3,11 +3,23 @@ import {UserService} from '../user.service';
 import {OrderRestAPI} from '../rest_api/order.restapi';
 import {Order} from '../models/Order';
 import {AsyncPipe} from '@angular/common';
+import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatDivider} from '@angular/material/divider';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatButton, MatIconButton} from '@angular/material/button';
 
 @Component({
   selector: 'profile',
   imports: [
-    AsyncPipe
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatIconButton,
+    MatButton
   ],
   templateUrl: 'profile.html'
 })
