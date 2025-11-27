41c165abe1c8f2d4e755c301a9e4f9eee6a62a10
IslamHamada <Islamhamada222@gmail.com>
2025-11-27 12:25:01 +0100

    add addToCartClick() implementation + remove its unneeded parameter

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,6 +10,10 @@ import {
   MatCardXlImage
 } from '@angular/material/card';
 import {MatButton} from '@angular/material/button';
+import {CartRestAPI} from '../rest_api/cart.restapi';
+import {UserService} from '../user.service';
+import {CartItem} from '../models/CartItem';
+import {SessionService} from '../session/session.service';
 
 @Component({
   selector: 'product-component',
@@ -27,6 +31,9 @@ import {MatButton} from '@angular/material/button';
 export class ProductComponent {
   product : Product | undefined;
   productRestAPI = inject(ProductRestAPI);
+  cartRestAPI = inject(CartRestAPI);
+  userService = inject(UserService);
+  sessionService = inject(SessionService);
   route = inject(ActivatedRoute)
   id = Number(this.route.snapshot.paramMap.get('id'));
 
@@ -39,7 +46,22 @@ export class ProductComponent {
     )
   }
 
-  protected addToCartClick(param: {}) {
-
+  protected addToCartClick() {
+    if(this.product) {
+      this.userService.user.cartItemCount++;
+      if (this.userService.user.loggedIn) {
+        this.userService.rxOnBackendId$(z => this.cartRestAPI.addCartItem(this.product?.id ?? -1, 1, z)).subscribe();
+      } else {
+        let cartItem: CartItem = {
+          cart_item_id: -1,
+          cart_item_count: 1,
+          product_id: this.product.id,
+          product_price: this.product.price,
+          product_name: this.product.name,
+          product_image: this.product.image,
+        }
+        this.sessionService.addCartItem(cartItem)
+      }
+    }
   }
 }
