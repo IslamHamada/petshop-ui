870b131048d882e1bbaaa085d076209ee9c43f44
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 11:43:22 +0100

    add profile while ordering to include in the order
    add address, first name, last name, phone-number

--- src/app/rest_api/order.restapi.ts
+++ src/app/rest_api/order.restapi.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Order} from '../models/Order';
 import {environment} from '../../environments/environment'
+import {UserProfile} from '../models/UserProfile';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ import {environment} from '../../environments/environment'
 export class OrderRestAPI {
   http = inject(HttpClient);
 
-  order(backend_id : number){
-    return this.http.post<Order>(`${environment.gateway_url}/order/${backend_id}`, {})
+  order(backend_id : number, profile : UserProfile){
+    return this.http.post<Order>(`${environment.gateway_url}/order/${backend_id}`, profile)
   }
 
   getOrderHistory(backend_id: number){
