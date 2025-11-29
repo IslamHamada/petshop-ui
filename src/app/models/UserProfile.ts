8164d60baa228c3bb76e1b96c0044cbb114684ed
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 11:53:43 +0100

    add UserProfile interface

---
+++ src/app/models/UserProfile.ts
@@ -0,0 +1,13 @@
+export interface UserProfile {
+  email: string,
+  username: string,
+  createdAt: string,
+  phoneNumber: string,
+  firstName: string,
+  lastName: string,
+  city: string,
+  country: string,
+  houseNumber: string,
+  postalCode: string,
+  street: string,
+}
