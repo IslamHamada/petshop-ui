99123fe5b8afd270d851e0f462068f6a54428c65
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 11:41:27 +0100

    add getUserProfile() and saveUserProfile() Rest endpoints

--- src/app/rest_api/user.restapi.ts
+++ src/app/rest_api/user.restapi.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../models/User';
 import {environment} from '../../environments/environment'
+import {UserProfile} from '../models/UserProfile';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,13 @@ export class UserRestAPI {
       username: user.username,
       email: user.email,
     });
+  }
+
+  getUserProfile(user_id : number) {
+    return this.http.get<UserProfile>(`${environment.gateway_url}/user/${user_id}`);
+  }
+
+  saveUserProfile(user_id : number, user_profile: UserProfile) {
+    return this.http.put<UserProfile>(`${environment.gateway_url}/user/${user_id}`, user_profile)
   }
 }
