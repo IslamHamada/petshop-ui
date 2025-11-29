139796db50708285be1468dfe8c40d857ba97d32
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 12:22:57 +0100

    add userProfile, initialize it and send it along with the order request

--- src/app/checkout/checkout.ts
+++ src/app/checkout/checkout.ts
@@ -6,6 +6,8 @@ import {Order} from '../models/Order';
 import {MatLabel,
         MatFormField} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
+import {UserProfile} from '../models/UserProfile';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'checkout',
@@ -16,14 +18,50 @@ import {MatInput} from '@angular/material/input';
     MatFormField,
     MatLabel,
     MatInput
+    RouterLink
   ]
 })
 export class CheckoutComponent{
   userService = inject(UserService);
   orderRestAPI = inject(OrderRestAPI);
+  userProfile: UserProfile = {
+    email: '',
+    username: '',
+    firstName: '',
+    lastName: '',
+    city: '',
+    country: '',
+    houseNumber: '',
+    postalCode: '',
+    street: '',
+    createdAt: '',
+    phoneNumber: ''
+  };
+  router = inject(Router);
+
+  ngOnInit(){
+    this.userService.rxOnBackendId$<UserProfile>(id => this.userRestAPI.getUserProfile(id)).subscribe(
+      profile => {
+        this.form.patchValue({
+          firstName: profile.firstName,
+          lastName: profile.lastName,
+          phoneNumber: profile.phoneNumber,
+          country: profile.country,
+          city: profile.city,
+          postalCode: profile.postalCode,
+          street: profile.street,
+          houseNumber: profile.houseNumber,
+        });
+      }
+    );
+  }
 
   orderClick(){
     this.userService.rxOnBackendId$<Order>(id => this.orderRestAPI.order(id)).subscribe();
     this.userService.user.cartItemCount = 0;
+    this.userService.rxOnBackendId$<Order>(id => this.orderRestAPI.order(id, this.userProfile)).subscribe(
+      () => this.userService.user.cartItemCount = 0
+    )
+    this.router.navigate(['/']);
   }
 }
