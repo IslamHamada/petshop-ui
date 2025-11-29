77aed4e3feb90f7961719e4e3e9de2d8b941e9c5
IslamHamada <Islamhamada222@gmail.com>
2025-11-29 11:54:13 +0100

    add more vars to Order interface

--- src/app/models/Order.ts
+++ src/app/models/Order.ts
@@ -3,5 +3,13 @@ import {OrderItem} from './OrderItem';
 export interface Order {
   time: string;
   price: number;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  country: string;
+  city: string;
+  street: string;
+  postalCode: string;
+  houseNumber: string;
   elaborateOrderItems: OrderItem[];
 }
