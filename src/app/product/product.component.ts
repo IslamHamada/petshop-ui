fae5c0894453bb18f15dcbd596f2382a2b2909cf
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 19:39:22 +0100

    add two chips, utility and for_animal, to each product page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -14,6 +14,7 @@ import {CartRestAPI} from '../rest_api/cart.restapi';
 import {UserService} from '../user.service';
 import {CartItem} from '../models/CartItem';
 import {SessionService} from '../session/session.service';
+import {MatChip, MatChipSet} from '@angular/material/chips';
 
 @Component({
   selector: 'product-component',
@@ -24,7 +25,9 @@ import {SessionService} from '../session/session.service';
     MatCardActions,
     MatCardContent,
     MatCardHeader,
-    MatCardXlImage
+    MatCardXlImage,
+    MatChip,
+    MatChipSet
   ],
   styleUrls: ['./product_component.sass']
 })
