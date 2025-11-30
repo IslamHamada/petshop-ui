f510a32a2bd807007e53a64bba80a849a36d4cb9
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 23:11:53 +0100

    remove repeated template import

--- src/app/product_list/product_list.ts
+++ src/app/product_list/product_list.ts
@@ -24,7 +24,6 @@ import {MatInputModule} from '@angular/material/input';
     MatPaginatorModule,
     MatInputModule,
     MatFormFieldModule,
-    FormsModule,
     RouterLink,
     MatChipsModule,
     FormsModule,
