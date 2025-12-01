359e726d7ce25f294950cedf6974463f92b47de3
IslamHamada <Islamhamada222@gmail.com>
2025-12-01 18:10:36 +0100

    move profile component to components directory + add init_loading + update_profile_loading + correct a sass selector and add 1rem bottom margin
    init_loading tracks if the rest calls in ngOnInit() are done
    update_profile_loading tracks if the rest call to update the user profile is done

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,16 +1,18 @@
 import {Component, inject} from '@angular/core';
-import {UserService} from '../user.service';
-import {OrderRestAPI} from '../rest_api/order.restapi';
-import {Order} from '../models/Order';
+import {UserService} from '../../user.service';
+import {OrderRestAPI} from '../../injectables/rest/order.restapi';
+import {Order} from '../../models/Order';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {RouterLink} from '@angular/router';
-import {UserProfile} from '../models/UserProfile';
-import {UserRestAPI} from '../rest_api/user.restapi';
+import {UserProfile} from '../../models/UserProfile';
+import {UserRestAPI} from '../../injectables/rest/user.restapi';
 import {FormsModule} from '@angular/forms';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'profile',
@@ -21,7 +23,9 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatButtonModule,
     RouterLink,
     FormsModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
   ],
   templateUrl: 'profile.html',
   styleUrl: 'profile.sass',
@@ -46,18 +50,27 @@ export class ProfileComponent {
   updated = false
   orderHistory : Order[] = [];
 
+  init_loading = 2;
+  update_profile_loading = false;
+
   ngOnInit() {
     this.userService.rxOnBackendId$<UserProfile>(id => this.userRestAPI.getUserProfile(id)).subscribe(
-      userProfile => this.userProfile = userProfile
+      userProfile => {
+        this.userProfile = userProfile;
+        this.init_loading--;
+      }
     );
     this.userService.rxOnBackendId$<Order[]>(id => this.orderRestAPI.getOrderHistory(id)).subscribe(
-      orderHistory => this.orderHistory = orderHistory
+      orderHistory => {
+        this.orderHistory = orderHistory;
+        this.init_loading--;
+      }
     );
   }
 
   saveProfileClick(){
-    this.updated = false;
+    this.update_profile_loading = true;
     this.userService.rxOnBackendId$(id => this.userRestAPI.saveUserProfile(id, this.userProfile))
-      .subscribe(()=> this.updated = true);
+      .subscribe(()=> this.update_profile_loading = false);
   }
 }
