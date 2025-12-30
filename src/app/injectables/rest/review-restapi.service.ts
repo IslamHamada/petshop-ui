11c9edf0ac13bc9d4332eca0859fb49d189bddc1
IslamHamada <islamhamada222@gmail.com>
2025-12-30 08:08:50 +0100

    add ReviewRestAPI injectable, with 2 rest calls

---
+++ src/app/injectables/rest/review-restapi.service.ts
@@ -0,0 +1,19 @@
+import {inject, Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Review} from "../../models/Review";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ReviewRestAPI {
+    http = inject(HttpClient);
+
+    getReviewsByProductId(product_id : number) {
+        return this.http.get<Review[]>(`${environment.gateway_url}/review/product/${product_id}`)
+    }
+
+    submitReview(review: Review) {
+        return this.http.post<Review>(`${environment.gateway_url}/review}`, review);
+    }
+}
