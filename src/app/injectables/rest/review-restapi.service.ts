20c4f56d9fecc95e913c7a11261cc9adcc8985ce
IslamHamada <islamhamada222@gmail.com>
2026-01-11 13:25:32 +0100

    update paths of review rest endpoints according to which is public and which is protected

--- src/app/injectables/rest/review-restapi.service.ts
+++ src/app/injectables/rest/review-restapi.service.ts
@@ -11,18 +11,18 @@ export class ReviewRestAPI {
     http = inject(HttpClient);
 
     getReviewsByProductId(product_id : number) {
-        return this.http.get<Review[]>(`${environment.gateway_url}/review/product/${product_id}`)
+        return this.http.get<Review[]>(`${environment.gateway_url}/review/public/product/${product_id}`)
     }
 
     submitReview(review: Review) {
-        return this.http.post<Review>(`${environment.gateway_url}/review`, review);
+        return this.http.post<Review>(`${environment.gateway_url}/review/protected`, review);
     }
 
     getReviewByProductIdAndUserId(product_id: number, userId: number) {
-        return this.http.get<Review>(`${environment.gateway_url}/review/product/user/${product_id}/${userId}`);
+        return this.http.get<Review>(`${environment.gateway_url}/review/protected/product/user/${product_id}/${userId}`);
     }
 
     getReviewsSummaryByProductId(product_id : number) {
-        return this.http.get<ReviewSummary>(`${environment.gateway_url}/review/product/summary/${product_id}`);
+        return this.http.get<ReviewSummary>(`${environment.gateway_url}/review/public/product/summary/${product_id}`);
     }
 }
