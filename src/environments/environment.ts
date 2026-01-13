37ec3b2d5d713f71abe0f40f07001abe986df23b
IslamHamada <islamhamada222@gmail.com>
2026-01-13 17:48:00 +0100

    set local environment properly

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  gateway_url: 'https://34.54.212.8.sslip.io',
-  frontend_url: 'https://34.54.201.1.sslip.io'
+  gateway_url: 'http://localhost:9090',
+  frontend_url: 'http://localhost:8090',
 }
